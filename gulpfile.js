f0401b8346f7c3460ed8527180ba86a631af8443
Stephen Grice <stephengrice@live.com>
2020-10-18 20:33:46 -0400

    Change gulpfile somehow :)

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-// JS_SRC = [
-// 	"clueless/js/vendor/jquery-3.5.1.js",
-// 	"clueless/js/vendor/bootstrap.min.js",
-// 	"clueless/js/vendor/socket.io.js",
-// 	"clueless/js/main.js",
-// ];
-//
-// const {dest, series, src} = require('gulp');
-//
-// var concat      = require('gulp-concat'),
-//     uglify      = require('gulp-uglify'),
-//     sourcemaps  = require('gulp-sourcemaps');
+JS_SRC = [
+	"clueless/js/vendor/jquery-3.5.1.js",
+	"clueless/js/vendor/bootstrap.min.js",
+	"clueless/js/vendor/socket.io.js",
+	"clueless/js/main.js",
+];
+
+const {dest, series, src} = require('gulp');
+
+var concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    sourcemaps  = require('gulp-sourcemaps');
 
 function concatJS() {
-  // return src(JS_SRC)
-  //   .pipe(sourcemaps.init())
-  //   .pipe(concat('app.min.js'))
-  //   .pipe(uglify())
-  //   .pipe(sourcemaps.write())
-  //   .pipe(dest('build/js'));
+  return src(JS_SRC)
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(dest('build/js'));
 }
 
-// exports.build = series(
-//   concatJS
-// );
+exports.build = series(
+  concatJS
+);
