f09da0abeead38e2d4976210ba326366cdde2795
Stephen Grice <stephengrice@live.com>
2020-10-21 18:36:37 -0400

    Implement Character on the client side

--- clueless/static/js/main.js
+++ clueless/static/js/main.js
@@ -1,3 +1,28 @@
-function add(a,b) {
-  return a + b;
+// Clue-Less Main JavaScript File
+
+// const object functions as enum. Has multiple aliases
+const Color = {
+  DEFAULT: 'yellow',
+  YELLOW: 'yellow',
+  COLONEL_MUSTARD: 'yellow',
+
+  RED: 'red',
+  MISS_SCARLET: 'red',
+
+  PURPLE: 'purple',
+  PROFESSOR_PLUM: 'purple',
+
+  GREEN: 'green',
+  MR_GREEN: 'green',
+
+  WHITE: 'white',
+  MRS_WHITE: 'white',
+
+  BLUE: 'blue',
+  MRS_PEACOCK: 'blue',
+};
+class Character {
+  constructor() {
+    this.color = Color.DEFAULT;
+  }
 }
