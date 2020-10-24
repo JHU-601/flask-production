058cd96a3b6f5e75a20e89fe39f615c5897d51f2
Ken Johnson <ken.johnso93@gmail.com>
2020-10-24 12:55:21 -0500

    feature/protobuf-impl
    - adding protobuf .proto and generated python/js

---
+++ clueless/static/room.js
@@ -0,0 +1,27 @@
+// source: messages.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.Room');
+
+/**
+ * @enum {number}
+ */
+proto.Room = {
+  STUDY: 0,
+  HALL: 1,
+  LOUNGE: 2,
+  LIBRARY: 3,
+  BILLIARD: 4,
+  DINING: 5,
+  CONSERVATORY: 6,
+  BALLROOM: 7,
+  KITCHEN: 8
+};
+
