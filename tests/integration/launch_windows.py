26183ae7d2c701bc721ad04aa0eb6dd1984ba35a
Stephen Grice <stephengrice@live.com>
2020-12-09 21:48:08 -0500

    Add client-side handler for new Joined messsage

--- tests/integration/launch_windows.py
+++ tests/integration/launch_windows.py
@@ -93,16 +93,17 @@ if __name__ == '__main__':
     game_id = driver.find_element_by_css_selector('#lblGameId').get_attribute('innerHTML')
     print('Game ID: ' + game_id)
     # Join game on the other tabs
-    for i in range(1,6):
+    for i in range(1,3): # temp
         driver.switch_to_window(driver.window_handles[i])
         driver.find_element_by_css_selector('#txtGameId').send_keys(game_id)
         driver.find_element_by_css_selector('#btnJoinGame').click()
     # Register each player
-    for i in range(6):
+    for i in range(3): # temp
         driver.switch_to_window(driver.window_handles[i])
         driver.find_element_by_css_selector('#character' + str(i)).click()
         driver.find_element_by_css_selector('#txtDisplayName').send_keys('player' + str(i))
         driver.find_element_by_css_selector('#btnRegister').click()
+    import sys; sys.exit(1) # temp
     # Go back to tab 0
     driver.switch_to_window(driver.window_handles[0])
     for i in range(6):
